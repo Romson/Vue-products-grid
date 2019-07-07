e5fc2ece1fb64f64ab251b458c89095ec5db9e34
romson <romerolijkwan@gmail.com>
2019-07-07 18:45:37 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,373 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <!-- pname -->
+      <div class="product-card">
+        <div class="badge">Sale</div>
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- pname -->
+      <div class="product-card">
+        <div class="badge">Hot</div>
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- pname -->
+      <div class="product-card">
+        <div class="badge">Aanbieding</div>
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- pname -->
+      <div class="product-card">
+        <div class="badge">Discount</div>
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- pname -->
+      <div class="product-card">
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- pname -->
+      <div class="product-card">
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- pname -->
+      <div class="product-card">
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- pname -->
+      <div class="product-card">
+        <div class="product-tumb">
+          <img src="https://i.imgur.com/SyMMixQ.png" alt />
+        </div>
+        <div class="product-details">
+          <span class="product-catagory">Tag,tags</span>
+          <h4>
+            <a href>Product</a>
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            possimus nostrum!
+          </p>
+          <div class="product-bottom-details">
+            <div class="product-price">
+              <small>€10.00</small>€7.45
+            </div>
+            <div class="product-links">
+              <a href>
+                <i class="fa fa-shopping-cart"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-rows: 1fr;
+  grid-gap: 20px;
+  width: 90%;
+  margin: 40px auto;
+}
+
+@import url("https://fonts.googleapis.com/css?family=Roboto:400,500,700");
+* {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+}
+a {
+  text-decoration: none;
+}
+
+.product-card {
+  box-shadow: 0 2px 7px #dfdfdf;
+  background: #fafafa;
+}
+
+.badge {
+  position: absolute;
+  left: 10;
+  top: 40px;
+  text-transform: uppercase;
+  font-size: 13px;
+  font-weight: 700;
+  background: red;
+  color: #fff;
+  padding: 3px 10px;
+}
+
+.product-tumb {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 250px;
+  padding: 20px;
+  background: #f0f0f0;
+}
+
+.product-tumb img {
+	max-width: 100%;
+	max-height: 100%;
+	transition: all 1s;
+}
+
+.product-tumb:hover img {
+	transform: scale(1.1);
+}
+
+.product-details {
+  padding: 30px;
+}
+
+.product-catagory {
+  display: block;
+  font-size: 12px;
+  font-weight: 700;
+  text-transform: uppercase;
+  color: #ccc;
+  margin-bottom: 18px;
+}
+
+.product-details h4 a {
+  font-weight: 500;
+  display: block;
+  margin-bottom: 18px;
+  text-transform: uppercase;
+  color: #363636;
+  text-decoration: none;
+  transition: 0.3s;
+}
+
+.product-details h4 a:hover {
+  color: #fbb72c;
+}
+
+.product-details p {
+  font-size: 15px;
+  line-height: 22px;
+  margin-bottom: 18px;
+  color: #999;
+}
+
+.product-bottom-details {
+  overflow: hidden;
+  border-top: 1px solid #eee;
+  padding-top: 20px;
+}
+
+.product-bottom-details div {
+  float: left;
+  width: 50%;
+}
+
+.product-price {
+  font-size: 18px;
+  color: #fbb72c;
+  font-weight: 600;
+}
+
+.product-price small {
+  font-size: 80%;
+  font-weight: 400;
+  text-decoration: line-through;
+  /* display: inline-block; */
+  margin-right: 5px;
+}
+
+.product-links {
+  text-align: right;
+}
+
+.product-links a {
+  /* display: inline-block; */
+  margin-left: 5px;
+  color: #e1e1e1;
+  transition: 0.3s;
+  font-size: 17px;
+}
+
+.product-links a:hover {
+  color: #fbb72c;
 }
 </style>
